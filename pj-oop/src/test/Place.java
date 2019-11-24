0acf1a194a6ffb8242200df15d2c224d93251685
Pavaree Petchaustanakul <61070161@kmitl.ac.th>
2019-11-24 18:35:27 +0700

    edit img in Place
    change floder .australia

--- pj-oop/src/test/Place.java
+++ pj-oop/src/test/Place.java
@@ -39,8 +39,15 @@ public class Place extends javax.swing.JInternalFrame {
 
     public void showInfo() {
         String path = "../place_image/australia/bondibeach.jpg";
-        InputStream imgStream;
-
+        
+        if (page.place.equals("Australia")){
+            img1.setIcon(new ImageIcon("src\\place_image_australia\\bondibeach.jpg"));
+            img1.setText("enter text here");
+            img2.setIcon(new ImageIcon("src\\place_image_australia\\12apostles.jpg"));
+            img2.setText("enter text here");
+            img3.setIcon(new ImageIcon("src\\place_image_australia\\sydneyopera.jpg"));
+            img3.setText("enter text here");
+        }
     }
 
     /**
@@ -52,9 +59,8 @@ public class Place extends javax.swing.JInternalFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel3 = new javax.swing.JPanel();
-        jLabel2 = new javax.swing.JLabel();
-        img1 = new javax.swing.JLabel();
+        Page_place = new javax.swing.JPanel();
+        txt_recommended = new javax.swing.JLabel();
         name1 = new javax.swing.JLabel();
         season1 = new javax.swing.JLabel();
         img2 = new javax.swing.JLabel();
@@ -63,19 +69,17 @@ public class Place extends javax.swing.JInternalFrame {
         img3 = new javax.swing.JLabel();
         name3 = new javax.swing.JLabel();
         season3 = new javax.swing.JLabel();
+        img1 = new javax.swing.JLabel();
 
         setPreferredSize(new java.awt.Dimension(930, 630));
 
-        jPanel3.setBackground(new java.awt.Color(3, 151, 161));
-        jPanel3.setPreferredSize(new java.awt.Dimension(930, 630));
-        jPanel3.setRequestFocusEnabled(false);
+        Page_place.setBackground(new java.awt.Color(3, 151, 161));
+        Page_place.setPreferredSize(new java.awt.Dimension(930, 630));
+        Page_place.setRequestFocusEnabled(false);
 
-        jLabel2.setFont(new java.awt.Font("Harlow Solid Italic", 0, 48)); // NOI18N
-        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel2.setText("Recommended Place");
-
-        img1.setText("img1");
-        img1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        txt_recommended.setFont(new java.awt.Font("Harlow Solid Italic", 0, 48)); // NOI18N
+        txt_recommended.setForeground(new java.awt.Color(255, 255, 255));
+        txt_recommended.setText("Recommended Place");
 
         name1.setText("name1");
 
@@ -93,81 +97,82 @@ public class Place extends javax.swing.JInternalFrame {
 
         season3.setText("season3");
 
-        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
-        jPanel3.setLayout(jPanel3Layout);
-        jPanel3Layout.setHorizontalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel3Layout.createSequentialGroup()
+        img1.setText("img1");
+        img1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+
+        javax.swing.GroupLayout Page_placeLayout = new javax.swing.GroupLayout(Page_place);
+        Page_place.setLayout(Page_placeLayout);
+        Page_placeLayout.setHorizontalGroup(
+            Page_placeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(Page_placeLayout.createSequentialGroup()
                 .addGap(255, 255, 255)
-                .addComponent(jLabel2)
+                .addComponent(txt_recommended)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-            .addGroup(jPanel3Layout.createSequentialGroup()
-                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel3Layout.createSequentialGroup()
+            .addGroup(Page_placeLayout.createSequentialGroup()
+                .addGroup(Page_placeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(Page_placeLayout.createSequentialGroup()
                         .addGap(103, 103, 103)
-                        .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addGroup(Page_placeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addComponent(name1)
                             .addComponent(season1)))
-                    .addGroup(jPanel3Layout.createSequentialGroup()
+                    .addGroup(Page_placeLayout.createSequentialGroup()
                         .addGap(41, 41, 41)
                         .addComponent(img1, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addGap(125, 125, 125)
-                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel3Layout.createSequentialGroup()
+                .addGroup(Page_placeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(Page_placeLayout.createSequentialGroup()
                         .addGap(62, 62, 62)
-                        .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addGroup(Page_placeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addComponent(name2)
                             .addComponent(season2)))
                     .addComponent(img2, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 108, Short.MAX_VALUE)
-                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel3Layout.createSequentialGroup()
+                .addGroup(Page_placeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(Page_placeLayout.createSequentialGroup()
                         .addGap(62, 62, 62)
-                        .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(Page_placeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(season3)
                             .addComponent(name3)))
                     .addComponent(img3, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(56, 56, 56))
         );
-        jPanel3Layout.setVerticalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel3Layout.createSequentialGroup()
+        Page_placeLayout.setVerticalGroup(
+            Page_placeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(Page_placeLayout.createSequentialGroup()
                 .addGap(72, 72, 72)
-                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(txt_recommended, javax.swing.GroupLayout.PREFERRED_SIZE, 87, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel3Layout.createSequentialGroup()
+                .addGroup(Page_placeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(Page_placeLayout.createSequentialGroup()
                         .addComponent(img1, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(name1)
                         .addGap(50, 50, 50)
                         .addComponent(season1))
-                    .addGroup(jPanel3Layout.createSequentialGroup()
+                    .addGroup(Page_placeLayout.createSequentialGroup()
                         .addComponent(img2, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(name2)
                         .addGap(50, 50, 50)
                         .addComponent(season2))
-                    .addGroup(jPanel3Layout.createSequentialGroup()
+                    .addGroup(Page_placeLayout.createSequentialGroup()
                         .addComponent(img3, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addComponent(name3)
                         .addGap(50, 50, 50)
                         .addComponent(season3)))
-                .addContainerGap(106, Short.MAX_VALUE))
+                .addContainerGap(134, Short.MAX_VALUE))
         );
-
-        img1.getAccessibleContext().setAccessibleName("img1");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(Page_place, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, 589, Short.MAX_VALUE)
+            .addComponent(Page_place, javax.swing.GroupLayout.DEFAULT_SIZE, 600, Short.MAX_VALUE)
         );
 
         pack();
@@ -175,16 +180,16 @@ public class Place extends javax.swing.JInternalFrame {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel Page_place;
     private javax.swing.JLabel img1;
     private javax.swing.JLabel img2;
     private javax.swing.JLabel img3;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JPanel jPanel3;
     private javax.swing.JLabel name1;
     private javax.swing.JLabel name2;
     private javax.swing.JLabel name3;
     private javax.swing.JLabel season1;
     private javax.swing.JLabel season2;
     private javax.swing.JLabel season3;
+    private javax.swing.JLabel txt_recommended;
     // End of variables declaration//GEN-END:variables
 }
