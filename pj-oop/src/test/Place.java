79f5db75858d285208ea0e2406f7dae7d373cba4
toeyratsit <rattasit15408@gmail.com>
2019-11-29 13:21:57 +0700

    place

--- pj-oop/src/test/Place.java
+++ pj-oop/src/test/Place.java
@@ -44,54 +44,32 @@ public class Place extends javax.swing.JInternalFrame {
             img1.setIcon(new ImageIcon("src\\pic\\aus\\12apostles.jpg"));
             name1.setText("Apostles");
             season1.setText("ช่วงเวลาที่เหมาะสม ฤดูร้อน");
-            jLabel1.setText("เสาหินปูนที่ตั้งเรียงรายอย่างตระหง่านนอกขายฝั่งรัฐวิคตอเรียนี้จริงๆ <br />"
-                    + "นับได้เพียงเก้าต้น เท่านั้นแต่ว่าตั้งชื่อตามคริสต์ศาสนาว่ามีสิบสองต้นเพื่อวัตถุประสงค์ทางการท่องเที่ยว\n"
-                    + " การกัดเซาะของ น้ำทะเลทำให้เสาต้นหนึ่งล้มไปแล้ว ส่วนที่เหลือก็คาดว่าจะประสบชะตากรรมเช่นเดียวกัน ลองเข้าไป\n"
-                    + "ชมเสาเหล่านี้ได้จากถนนเกรท โอเชียน (Great Ocean Road) เขตเมืองเมลเบิร์น ซึ่งเป็นถนนขับรถ ชมวิวที่สวยที่สุดในทวีป ");
             img2.setIcon(new ImageIcon("src\\pic\\aus\\bondibeach.jpg"));
             name2.setText("Bondibeach");
             season2.setText("ช่วงเวลาที่เหมาะสม ฤดูร้อน");
-            jLabel2.setText("ชายหาดบอนไดเป็นชายหาดที่เป็นเอกลักษณ์ที่มีชื่อเสียงมากที่สุดแห่งหนึ่งไม่เพียงแต่ใน ออสเตรเลียเท่านั้นแต่ทั่วโลก"
-                    + "เป็นจุดพักผ่อนยอดนิยมในท้องถิ่นทั้งสำหรับการเล่นกระดานโต้คลื่น ว่ายน้ำและอาบแดด ชายหาดที่เป็นสวรรค์สำหรับการออกมาดูผู้คนทำกิจกรรมนี้ยังมีพื้นที่สวนสาธารณะด้านหลังที่มีนักเล่น "
-                    + " โรลเลอร์เบลดและสเก็ตมาโชว์ทักษะการเล่นให้ได้ชมกันอีกด้วย ");
             img3.setIcon(new ImageIcon("src\\pic\\aus\\sydneyopera.jpg"));
             name3.setText("Sydneyopera");
             season3.setText("ช่วงเวลาที่เหมาะสม ช่วงคริสต์มาสถึงปีใหม่");
-            jLabel3.setText("เป็นสัญลักษณ์ของออสเตรเลียที่รู้จักกันทั่วโลกอย่างไม่ต้องสงสัย คุณสามารถแค่ถ่ายรูปภาพอาคารทรง ใบเรือที่มีชื่อเสียง เข้าไปนั่งฟังโอเปร่า หรือชมดอกไม้ไฟจากเรือในท่าเรือซิดนีย์ตอนเที่ยงคืนในคืน ก่อนปีใหม่ก็ได้ ");
         } else if (page.place.equals("Canada")) {
             img1.setIcon(new ImageIcon("src\\pic\\canada\\banff-national-park.jpg"));
             name1.setText("Banff National Park");
             season1.setText("ช่วงเวลาที่เหมาะสม ฤดูร้อน");
-            jLabel1.setText("เป็นสถานที่ท่องเที่ยวยอด นิยมมากที่สุดแห่งหนึ่งของแคนาดา ด้วยที่นี่มีพื้นที่ส่วนหนึ่งอยู่ในแนวเทือกเขาร็อกกี เมาน์เทน และมี "
-                    + " จุดชมวิวเทือกเขาร็อกกี เมาน์เทน ที่สวยที่สุดอีกด้วย ทะเลสาบก็ไม่น้อยหน้า มี ความงดงามติดอันดับต้น ๆ ของโลกเลยทีเดียว นั่นก็คือ Lake Louise มีลักษณะเป็นทะเลสาบสีฟ้า "
-                    + " เขียวมรกตสวยใส รายล้อมไปด้วยเทือกเขาสีเขียวสูงใหญ่");
             img2.setIcon(new ImageIcon("src\\pic\\canada\\niagara-falls.jpg"));
             name2.setText("Niagara Falls");
             season2.setText("ช่วงเวลาที่เหมาะสม ฤดูร้อน");
-            jLabel2.setText("าเป็นหนึ่งในน้ำตกที่ยิ่งใหญ่ที่สุด ในโลก ตั้งอยู่ระหว่างชายแดนรัฐนิวยอร์ก ประเทศสหรัฐอเมริกา และรัฐออนตาริโอ ประเทศแคนาดา "
-                    + " ซึ่งน้ำตกแห่งนี้จะประกอบด้วย 3 น้ำตกด้วยกัน คือน้ำตกเกือกม้า (Horseshoe Falls) อยู่ทางฝั่ง แคนาดา, น้ำตกอเมริกา (American Falls) อยู่ทางฝั่งสหรัฐอเมริกา "
-                    + " และน้ำตก Bridal Veil อยู่ทางฝั่ง อเมริกาเช่นกัน");
             img3.setIcon(new ImageIcon("src\\pic\\canada\\ottawa.jpg"));
             name3.setText("Ottawa");
             season3.setText("ช่วงเวลาที่เหมาะสม ทุกช่วงฤดูกาล");
-            jLabel3.setText("เป็น เมืองหลวงที่มีเสน่ห์มากที่สุดแห่งหนึ่งของโลก ใครที่หลงใหลในสถาปัตยกรรมฟื้นฟูโกธิค (Gothic Revival architecture) จะต้องหลงรักเมืองแห่งนี้แน่นอน"
-                    + " ที่นี่ไม่ค่อยมีตึกและอาคารสูง ๆ รูปแบบทันสมัยเยอะ มากนัก รอบ ๆ เมืองเราจะได้เห็นอาคารในรูปแบบสถาปัตยกรรมฟื้นฟูโกธิคเสียเป็นส่วนใหญ่");
         } else if (page.place.equals("China")) {
             img1.setIcon(new ImageIcon("src\\pic\\china\\798 Art District_5.jpg"));
             name1.setText("798 Art District");
             season1.setText("ช่วงเวลาที่เหมาะสม ทุกช่วงฤดูกาล");
-            jLabel1.setText("เป็นชุมชนศิลปินขนาดใหญ่ที่มีชื่อเสียงมากของจีน ตั้งอยู่ใจกลางกรุงปักกิ่ง\n" +
-"สมัยก่อนโรงงาน 798 แห่งนี้เคยเป็นที่ตั้งของโรงงานผลิตอุปกรณ์ไฟฟ้า และอิเล็กทรอนิกส์ ของทหาร\n" +
-"โดยจะเป็นชุมชนศิลปะ งานอาร์ต ร้านของแฮนเมดต่างๆ");
             img2.setIcon(new ImageIcon("src\\pic\\china\\Great Wall of China_3.jpg"));
             name2.setText("Great Wall of China");
             season2.setText("ช่วงเวลาที่เหมาะสม ฤดูใบไม้ผลิ และฤดูหนาว");
-            jLabel2.setText("เป็นสิ่งมหัศจรรย์ของโลกที่มนุษย์สร้างขึ้น โดยเป็นกำแพงที่มีความยาวถึง 21,196.18 กิโลเมตร อย่างที่เรียกกันว่า กําแพงหมื่นลี้ ครอบคลุมพื้นที่ 15 มณฑลทั่วประเทศจีน");
             img3.setIcon(new ImageIcon("src\\pic\\china\\Olympic Stadium 2008_3.jpg"));
             name3.setText("The Beijing National Stadium");
             season3.setText("ช่วงเวลาที่เหมาะสม ทุกช่วงฤดูกาล");
-            jLabel3.setText("โดยสนามแห่งนี้ได้ใช้ในมหกรรมกีฬาโอลิมปิกในปี 2008 พองานกีฬาได้สิ้นสุดลง\n"
-                    + "ก็ได้เปิดเป็นสถานที่ท่องเที่ยวให้นักท่องเที่ยวเข้ามาชม ถือว่าถ้ามาปักกิ่งแล้วต้องห้ามพลาดค่ะ");
         } else if (page.place.equals("Croatia")) {
             img1.setIcon(new ImageIcon("src\\pic\\croatia\\dubrovnikwalls.jpg"));
             name1.setText("Dubrovnik Old Town");
@@ -277,9 +255,6 @@ public class Place extends javax.swing.JInternalFrame {
         name3 = new javax.swing.JLabel();
         season3 = new javax.swing.JLabel();
         img1 = new javax.swing.JLabel();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
 
         setPreferredSize(new java.awt.Dimension(930, 630));
 
@@ -314,12 +289,6 @@ public class Place extends javax.swing.JInternalFrame {
 
         img1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
 
-        jLabel1.setText("jLabel2");
-
-        jLabel2.setText("jLabel1");
-
-        jLabel3.setText("jLabel3");
-
         javax.swing.GroupLayout Page_placeLayout = new javax.swing.GroupLayout(Page_place);
         Page_place.setLayout(Page_placeLayout);
         Page_placeLayout.setHorizontalGroup(
@@ -333,7 +302,7 @@ public class Place extends javax.swing.JInternalFrame {
                 .addGroup(Page_placeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(Page_placeLayout.createSequentialGroup()
                         .addComponent(img1, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 86, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 40, Short.MAX_VALUE)
                         .addComponent(img2, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(57, 57, 57)
                         .addComponent(img3, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -343,23 +312,15 @@ public class Place extends javax.swing.JInternalFrame {
                                 .addComponent(name1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addGap(56, 56, 56))
                             .addGroup(Page_placeLayout.createSequentialGroup()
-                                .addGroup(Page_placeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(season1, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addComponent(season1, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(96, 96, 96)))
                         .addGroup(Page_placeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(Page_placeLayout.createSequentialGroup()
-                                .addGroup(Page_placeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(name2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(season2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                                .addGap(57, 57, 57))
-                            .addGroup(Page_placeLayout.createSequentialGroup()
-                                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                            .addComponent(name2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(season2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(57, 57, 57)
                         .addGroup(Page_placeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(name3, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(season3, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                            .addComponent(season3, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addGap(47, 47, 47))
         );
         Page_placeLayout.setVerticalGroup(
@@ -382,13 +343,7 @@ public class Place extends javax.swing.JInternalFrame {
                     .addComponent(season2)
                     .addComponent(season3)
                     .addComponent(season1, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(Page_placeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(Page_placeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(29, 29, 29))
+                .addGap(91, 91, 91))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -411,9 +366,6 @@ public class Place extends javax.swing.JInternalFrame {
     private javax.swing.JLabel img1;
     private javax.swing.JLabel img2;
     private javax.swing.JLabel img3;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel name1;
     private javax.swing.JLabel name2;
     private javax.swing.JLabel name3;
